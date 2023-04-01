07fc830c1e791559505d7b068b40ac8308fb7c9e
sujamess <surarerk.boonjit@gmail.com>
2023-04-02 02:36:12 +0700

    benchmark: add cheetah benchmark

--- cheetah_test.go
+++ cheetah_test.go
@@ -1,6 +1,7 @@
 package cheetah
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -64,5 +65,80 @@ func TestCheetah(t *testing.T) {
 		if recorder.Body.String() != tc.body {
 			t.Errorf("Expected response body %q, but got %q", tc.body, recorder.Body.String())
 		}
+	}
+}
+
+func BenchmarkCheetahGet(b *testing.B) {
+	c := New()
+	c.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, Cheetah!")
+	})
+
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.ServeHTTP(w, req)
+	}
+}
+
+func BenchmarkCheetahPost(b *testing.B) {
+	c := New()
+	c.Post("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Cheetah is running!")
+	})
+
+	req, _ := http.NewRequest(http.MethodPost, "/", nil)
+	w := httptest.NewRecorder()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.ServeHTTP(w, req)
+	}
+}
+
+func BenchmarkCheetahPut(b *testing.B) {
+	c := New()
+	c.Put("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Cheetah is resting!")
+	})
+
+	req, _ := http.NewRequest(http.MethodPut, "/", nil)
+	w := httptest.NewRecorder()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.ServeHTTP(w, req)
+	}
+}
+
+func BenchmarkCheetahPatch(b *testing.B) {
+	c := New()
+	c.Patch("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Cheetah is napping!")
+	})
+
+	req, _ := http.NewRequest(http.MethodPatch, "/", nil)
+	w := httptest.NewRecorder()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.ServeHTTP(w, req)
+	}
+}
+
+func BenchmarkCheetahDelete(b *testing.B) {
+	c := New()
+	c.Delete("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Cheetah is eating!")
+	})
+
+	req, _ := http.NewRequest(http.MethodDelete, "/", nil)
+	w := httptest.NewRecorder()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.ServeHTTP(w, req)
 	}
 }
