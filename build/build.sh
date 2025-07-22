43a7efef024ad611e41aa8015e85cd55172004e5
agocan <578843228@qq.com>
2025-07-22 09:58:18 +0800

    整理一下gitignore和build

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+BASH_NAME=$0
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+
+cd ${SCRIPT_DIR}/../internal/command/templates 
+git clone -b 0.0.8 https://github.com/go-cheetah/ansible-template.git ansible 
+git clone -b 0.0.8 https://github.com/go-cheetah/command-template.git command 
+git clone -b 0.0.8 https://github.com/go-cheetah/gitbook-template.git gitbook 
+git clone -b 0.0.8 https://github.com/go-cheetah/grps-go-template.git grps-go 
+git clone -b 0.0.8 https://github.com/go-cheetah/http-template.git http 
+git clone -b 0.0.8 https://github.com/go-cheetah/mdbook-template.git mdbook 
+git clone -b 0.0.8 https://github.com/go-cheetah/mvc-template.git mvc 
+git clone -b 0.0.8 https://github.com/go-cheetah/simple-template.git simple
+cd - 
+
 shot_ID=$(git rev-parse --short HEAD)
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X 'github.com/go-cheetah/cheetah/cmd/cheetah/app.Version=${shot_ID}'" -o output/cheetah-linux-amd64 cmd/cheetah/main.go
